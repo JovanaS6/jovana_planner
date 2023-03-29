118b55b6cf7886c3792a5a91843853ec2f9b518d
Jovana Savic <joocaasavic@gmail.com>
2023-03-29 09:22:00 +0200

    Kreiran EventFactory sa fakerom

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -17,7 +17,9 @@ class EventFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->sentence(5, true),
+            'date' => fake()->dateTimeBetween('now', '+90 days'),
+            'tickets' => fake()->numberBetween(10, 100000)
         ];
     }
 }
