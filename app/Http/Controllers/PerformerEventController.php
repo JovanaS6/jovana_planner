b0d9f9ed16f5c0e50ea622adb1560cf846eb39b7
Jovana Savic <joocaasavic@gmail.com>
2023-03-29 11:31:59 +0200

    Azuriran PerformerEvent controller

--- app/Http/Controllers/PerformerEventController.php
+++ app/Http/Controllers/PerformerEventController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\EventCollecton;
+use App\Models\Event;
+use App\Models\Performer;
+
 use Illuminate\Http\Request;
 
 class PerformerEventController extends Controller
 {
     //
+    public function index($performer_id)
+    {
+        $performer = Performer::find($performer_id);
+        if (is_null($performer)) {
+            return response()->json('Performer not found', 404);
+        }
+
+        $events = Event::get()->where('performer_id', $performer_id);
+        if (is_null($events)) {
+            return response()->json('Events not found', 404);
+        }
+
+        return response()->json([
+            'performer' => $performer->name,
+            'events' => new EventCollecton($events)
+        ]);
+    }
 }
