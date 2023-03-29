13b1e19309930e45aa4fa12361514462fd83e33e
Jovana Savic <joocaasavic@gmail.com>
2023-03-29 10:11:32 +0200

    Azuriran EventResource

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\LocationResource;
+use App\Http\Resources\PerformerResource;
+
 
 class EventResource extends JsonResource
 {
@@ -12,8 +15,19 @@ class EventResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
+
+     
+    public static $wrap = 'event';
+    
     public function toArray($request)
     {
-        return parent::toArray($request);
+       
+        return [
+            'name' => $this->resource->name,
+            'performer' => new PerformerResource($this->resource->performer),
+            'location' => new LocationResource($this->resource->location),
+            'date' => $this->resource->date,
+            'number of tickets' => $this->resource->tickets
+        ];
     }
 }
